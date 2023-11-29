6769f58c338ac80c72322b7943e5f1d3dc3292ee
Hasegawa-Akito <h.akito1999@gmail.com>
2023-11-29 22:46:22 +0900

    add: コメント・感想追加

--- src/03/05/index.ts
+++ src/03/05/index.ts
@@ -1,7 +1,15 @@
+// Errorクラスの派生クラスを作成し、Errorクラスを分けわかりやすくする
+// スローされたerrorはメッセージで分岐して処理を行うよりerrorクラスの種類によって分岐するほうが良さそう
+// if (error instanceof RangeError) {
+// }
+// みたいな感じで
 export class RangeError extends Error {}
+export class HttpError extends Error {}
 
 function checkRange(value: number) {
   if (value < 0 || value > 100) {
+    // 例外処理のメッセージはテストケースをふまえしっかりと明記する。
+    // メッセージを他と差別化することで期待した例外処理が行えているかテストで確認できる
     throw new RangeError("入力値は0〜100の間で入力してください");
   }
 }
