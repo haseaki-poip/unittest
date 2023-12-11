e4f3f094dd877fdff6346e22720d015f383c3cec
Hasegawa-Akito <h.akito1999@gmail.com>
2023-12-11 23:14:21 +0900

    add: コメント追加

--- src/05/03/Form.test.tsx
+++ src/05/03/Form.test.tsx
@@ -11,15 +11,22 @@ test("ボタンの表示", () => {
   expect(screen.getByRole("button")).toBeInTheDocument();
 });
 
+test("ボタンのテキスト取得", () => {
+  render(<Form name="taro" />);
+  expect(screen.getByRole("button")).toHaveTextContent("編集する");
+});
+
 test("見出しの表示", () => {
   render(<Form name="taro" />);
   expect(screen.getByRole("heading")).toHaveTextContent("アカウント情報");
 });
 
 test("ボタンを押下すると、イベントハンドラーが呼ばれる", () => {
+  // ボタンが押されたときの処理にモック関数としてスパイを入れる。p78, 04-5
   const mockFn = jest.fn();
   render(<Form name="taro" onSubmit={mockFn} />);
   fireEvent.click(screen.getByRole("button"));
+  // スパイから情報を確認
   expect(mockFn).toHaveBeenCalled();
 });
 
