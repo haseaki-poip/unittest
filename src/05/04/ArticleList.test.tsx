f64001a34898c140c07ebe57fee0bb221b2caaec
Hasegawa-Akito <h.akito1999@gmail.com>
2023-12-13 00:55:51 +0900

    add: コメント

--- src/05/04/ArticleList.test.tsx
+++ src/05/04/ArticleList.test.tsx
@@ -9,18 +9,27 @@ test("タイトルの表示", () => {
 
 test("items の数だけ一覧表示される", () => {
   render(<ArticleList items={items} />);
+  // getAllByRoleは概要する要素全てをが配列として取得
   expect(screen.getAllByRole("listitem")).toHaveLength(3);
 });
 
 test("items の数だけ一覧表示される", () => {
   render(<ArticleList items={items} />);
+  // roleのlistとはul要素のことである
   const list = screen.getByRole("list");
+  // ul要素があるかどうか
   expect(list).toBeInTheDocument();
+  // テスト対象ではないli要素を含まないように対象のul要素からliについて絞ってテスト
   expect(within(list).getAllByRole("listitem")).toHaveLength(3);
 });
 
 test("一覧アイテムが空のとき「投稿記事がありません」が表示される", () => {
   render(<ArticleList items={[]} />);
+  // getByRoleを使用するとエラーが出てしまいテストが中断される
+  // queryByRoleは存在しないものに対して要素を取得しようと試みることができる
+  // そのためエラーで中断されずに要素がないことの確認テストができる
+  // queryByRoleの返り値の方はHTMLElementまたはnullとなっていて存在しないことも許容していることがわかる
+  // 逆にgetByRoleはHTMLElementのみで許容していないことがわかる
   const list = screen.queryByRole("list");
   expect(list).not.toBeInTheDocument();
   expect(list).toBeNull();
